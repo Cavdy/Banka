fcf22026efb30038656c4d5b0f9d3f8e2dc0e2ba
Cavdy <cavdyofficials@gmail.com>
2019-03-18 05:40:36 +0100

    layout created

---
+++ UI/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    // Sidebar Toggle 
+    window.addEventListener("resize", () => {
+        if (window.matchMedia("(min-width: 900px)").matches) {
+            $(".sidebar").removeClass("sidebar-hide");
+        } else {
+            $(".sidebar").addClass("sidebar-hide");
+        }
+    });
+
+    $("#toggle-sidebar").click(() => {
+        $(".sidebar").toggleClass("sidebar-hide");
+    });
+});
