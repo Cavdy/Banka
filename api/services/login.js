8ad44b653b372346b4219f2402d7de2456ebf98d
cavdy <cavdyofficials@gmail.com>
2019-04-09 04:02:16 +0100

    feature(login route api): login route api with validation and authentication [Finishes #165203521]

--- api/services/login.js
+++ api/services/login.js
@@ -4,13 +4,32 @@ const { users } = usersData;
 
 const LoginService = {
   loginUser(userData) {
-    let receiveData = 'no account found';
-    for (let i = 0; i <= users.length - 1; i++) {
-      if (userData.email === users[i].email && userData.password === users[i].password) {
-        receiveData = userData;
+    const emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,10})$/;
+    const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    let returnValue = [];
+
+    // Check if email and password is valid
+    if (emailRegex.test(userData.email) && passwordRegex.test(userData.password)) {
+      let checkDetails = false;
+
+      // check if account exist
+      for (let i = 0; i <= users.length - 1; i++) {
+        if (users[i].email === userData.email && users[i].password === userData.password) {
+          checkDetails = true;
+        }
       }
+
+      // gives output
+      if (checkDetails) {
+        returnValue = userData;
+      } else {
+        returnValue.push('incorrect credentials');
+      }
+    } else {
+      returnValue.push('Invalid format');
     }
-    return receiveData;
+
+    return returnValue;
   },
 };
 
