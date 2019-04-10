ae011ff4261ef9fe62eb43d123d8dacce27ab1b5
cavdy <cavdyofficials@gmail.com>
2019-04-10 21:37:34 +0100

    feature(add jwt to login and create account route): protected createaccount route with jwt and use token to access user [Starts #165259978]

--- api/controllers/createAccount.js
+++ api/controllers/createAccount.js
@@ -1,13 +1,20 @@
+import jwt from 'jsonwebtoken';
 import CreateAccountService from '../services/createAccount';
 
 const CreateAccountController = {
   createAccount(req, res) {
     const accountData = req.body;
-    const createdAccount = CreateAccountService.createAccount(accountData);
-    return res.json({
-      status: 'success',
-      data: createdAccount,
-    }).status(201);
+    jwt.verify(req.token, '5634', (err, authorizedData) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        const createdAccount = CreateAccountService.createAccount(accountData, authorizedData);
+        return res.json({
+          status: 'success',
+          data: createdAccount,
+        }).status(201);
+      }
+    });
   },
 };
 
--- api/controllers/login.js
+++ api/controllers/login.js
@@ -1,13 +1,27 @@
+import jwt from 'jsonwebtoken';
 import LoginService from '../services/login';
 
 const LoginController = {
   loginUser(req, res) {
     const userData = req.body;
     const loggedUser = LoginService.loginUser(userData);
-    return res.json({
-      status: 'success',
-      data: loggedUser,
-    }).status(201);
+
+    return jwt.sign({ loggedUser }, '5634', (err, token) => {
+      if (err) { console.log(err); }
+      if (loggedUser[0] === 'Invalid format' || loggedUser[0] === 'incorrect credentials') {
+        res.json({
+          data: 'incorrect data',
+        });
+      } else {
+        res.json({
+          status: 'success',
+          data: {
+            loggedUser,
+            token,
+          },
+        }).status(201);
+      }
+    });
   },
 };
 
