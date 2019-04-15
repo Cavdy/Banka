3914a52f0e4b0a85c6e49855210ba37ff596ae0c
cavdy <cavdyofficials@gmail.com>
2019-04-15 22:08:57 +0100

    feature(create user profile page): create user profile page [Finishes #165361606]

--- ui/js/app.js
+++ ui/js/app.js
@@ -2,22 +2,22 @@
 const sidebar = document.querySelector('.sidebar');
 
 window.addEventListener('resize', () => {
-    if (window.matchMedia('(min-width: 900px)').matches) {
-        sidebar.classList.remove('sidebar-hide');
-    } else {
-        sidebar.classList.add('sidebar-hide');
-    }
+  if (window.matchMedia('(min-width: 900px)').matches) {
+    sidebar.classList.remove('sidebar-hide');
+  } else {
+    sidebar.classList.add('sidebar-hide');
+  }
 });
 
 if (window.innerWidth <= 600) {
-    sidebar.classList.add('sidebar-hide');
+  sidebar.classList.add('sidebar-hide');
 }
 
 document.querySelector('#toggle-sidebar').addEventListener('click', () => {
-    sidebar.classList.toggle('sidebar-hide');
-    if (window.innerWidth <= 600) {
-        document.querySelector('.user-nav-icon-box').toggleClass('user-nav-icon-box-active');
-    }
+  sidebar.classList.toggle('sidebar-hide');
+  if (window.innerWidth <= 600) {
+    document.querySelector('.user-nav-icon-box').toggleClass('user-nav-icon-box-active');
+  }
 });
 
 // Pop box
@@ -25,10 +25,10 @@ const userNavUserPop = document.querySelector('.user-nav-user-pop');
 const userNavNotificationPop = document.querySelector('.user-nav-notification-pop');
 
 const notificationPop = (element, removeClass, removeElement, toggleClass, toggleElement) => {
-    document.querySelector(element).addEventListener('click', () => {
-        removeClass.classList.remove(removeElement);
-        toggleClass.classList.toggle(toggleElement);
-    });
+  document.querySelector(element).addEventListener('click', () => {
+    removeClass.classList.remove(removeElement);
+    toggleClass.classList.toggle(toggleElement);
+  });
 }
 
 notificationPop('.user-nav-icon-box', userNavUserPop, 'user-nav-user-pop-active', userNavNotificationPop, 'user-nav-notification-pop-active');
@@ -37,30 +37,30 @@ notificationPop('.user-nav-user', userNavNotificationPop, 'user-nav-notification
 // dismiss alert
 const close = document.querySelector('#close');
 if (close !== null) {
-    close.addEventListener('click', () => {
-        document.querySelector('.alert').remove();
-    });
+  close.addEventListener('click', () => {
+    document.querySelector('.alert').remove();
+  });
 }
 
 // modal
 const showModal = document.querySelectorAll('#show-modal');
 
 if (showModal) {
-    const mModal = document.querySelector('.modal');
+  const mModal = document.querySelector('.modal');
 
-    showModal.forEach((modal) => {
-        modal.addEventListener('click', () => {
-                mModal.style.visibility = 'visible';
-                mModal.style.opacity = '1';
-        });
+  showModal.forEach((modal) => {
+    modal.addEventListener('click', () => {
+      mModal.style.visibility = 'visible';
+      mModal.style.opacity = '1';
     });
+  });
 }
 
 window.addEventListener('click', (e) => {
-    const modal = document.querySelector('#modal');
-    const cModal = document.querySelector('.modal');
-    if (e.target === modal) {
-        cModal.style.visibility = 'hidden';
-        cModal.style.opacity = '0';
-    }
+  const modal = document.querySelector('#modal');
+  const cModal = document.querySelector('.modal');
+  if (e.target === modal) {
+    cModal.style.visibility = 'hidden';
+    cModal.style.opacity = '0';
+  }
 });
