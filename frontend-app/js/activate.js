d0d9c3be7884def478ac41ef79611dca0662bfa0
cavdy <cavdyofficials@gmail.com>
2019-05-06 17:14:48 +0100

    feature(Add Modal to add staff):Add modal to add staff Add modal to add staff [Delivers #165789270]

--- frontend-app/js/activate.js
+++ frontend-app/js/activate.js
@@ -2,9 +2,11 @@ const token = sessionStorage.getItem('token');
 const api = 'https://bankaapp-api.herokuapp.com/api';
 const go = document.querySelector('#go');
 const limitSelect = document.querySelector('#limit');
-const onSuccess = document.querySelectorAll('.successMessage');
-const errMessage = document.querySelectorAll('.errMessage');
-const errMsg = document.querySelector('.errorMessage');
+const loader = document.querySelector('#loader');
+const successMsg = document.querySelector('.successMsg');
+const errMsg = document.querySelector('.errMsg');
+const userTable = document.querySelector('.user-table');
+const queryForm = document.querySelector('.queryForm');
 
 // POST FETCH REQUEST FOR ACCOUNTS
 const formSelect = document.querySelector('.form-select');
@@ -26,16 +28,17 @@ const patchApi = (url, data) => {
   })
     .then(response => response.json())
     .then((data1) => {
+      const cModal = document.querySelector('.modal');
       if (data1.status === 200) {
-        errMessage.forEach((err) => {
-          err.innerHTML = '';
-        });
-        onSuccess.forEach((success) => {
-          success.innerHTML = 'Updated successfully';
-          setInterval(() => {
-            document.location.reload(true);
-          }, 3000);
-        });
+        cModal.style.visibility = 'hidden';
+        cModal.style.opacity = '0';
+        errMsg.parentElement.style.display = 'none';
+        errMsg.innerHTML = '';
+        successMsg.parentElement.style.display = 'flex';
+        successMsg.innerHTML = 'Account updated Successfuly';
+        setInterval(() => {
+          document.location.reload(true);
+        }, 3000);
       }
     });
 };
@@ -54,17 +57,18 @@ const deleteApi = (url) => {
     redirect: 'follow',
     referrer: 'no-referrer',
   })
-    .then((response) => {
-      if (response.status === 204) {
-        errMessage.forEach((err) => {
-          err.innerHTML = '';
-        });
-        onSuccess.forEach((success) => {
-          success.innerHTML = 'Deleted successfully';
-          setInterval(() => {
-            document.location.reload(true);
-          }, 3000);
-        });
+    .then(response => response.json())
+    .then((data) => {
+      const cModal = document.querySelector('.modal');
+      if (data.status === 200) {
+        loader.style.display = 'none';
+        cModal.style.visibility = 'hidden';
+        cModal.style.opacity = '0';
+        successMsg.parentElement.style.display = 'flex';
+        successMsg.innerHTML = 'Account successfully deleted';
+        setInterval(() => {
+          location.reload(true);
+        }, 3000);
       }
     });
 };
@@ -85,17 +89,33 @@ const getAccountsApi = (url) => {
   })
     .then((response) => {
       if (response.status === 403) {
+        loader.style.display = 'none';
+        errMsg.parentElement.style.display = 'flex';
         errMsg.innerHTML = 'you must be logged in to view accounts';
+        userTable.style.display = 'none';
+        queryForm.style.display = 'none';
       } else {
+        errMsg.parentElement.style.display = 'none';
+        loader.style.display = 'none';
         errMsg.innerHTML = '';
+        userTable.style.display = 'block';
+        queryForm.style.display = 'flex';
         return response.json();
       }
     })
     .then((data1) => {
       if (data1.status === 401) {
+        loader.style.display = 'none';
+        errMsg.parentElement.style.display = 'flex';
         errMsg.innerHTML = 'you must be an admin or staff to view accounts';
+        userTable.style.display = 'none';
+        queryForm.style.display = 'none';
       } else {
+        errMsg.parentElement.style.display = 'none';
+        userTable.style.display = 'block';
+        queryForm.style.display = 'flex';
         data1.data.map((i) => {
+          loader.style.display = 'none';
           const table = document.querySelector('.table');
           const tableRow = document.createElement('div');
           tableRow.classList = 'table-row table-body';
@@ -121,6 +141,7 @@ const getAccountsApi = (url) => {
           const aDeleteAccount = document.createElement('a');
           aDeleteAccount.href = '#';
           aDeleteAccount.className = 'deactivate-btn';
+          aDeleteAccount.id = 'show-modal2';
           aDeleteAccount.innerHTML = 'Delete';
           deleteAccount.appendChild(aDeleteAccount);
           tableRow.appendChild(accountNumber);
@@ -131,11 +152,10 @@ const getAccountsApi = (url) => {
           table.appendChild(tableRow);
         });
       }
-      return data1;
-    })
-    .then(() => {
+
       // modal
       const showModal = document.querySelectorAll('#show-modal');
+      const showModal2 = document.querySelectorAll('#show-modal2');
       const accountName = document.querySelectorAll('#username');
       const accountNumber = document.querySelectorAll('#acnumber');
       const accountBalance = document.querySelectorAll('#balance');
@@ -143,29 +163,66 @@ const getAccountsApi = (url) => {
       const modalFunction = (modalClick, modalIn, modalId) => {
         if (modalClick) {
           const mModal = document.querySelector(modalIn);
+          const onPatch = document.querySelector('.onPatch');
+          const onDelete = document.querySelector('.onDelete');
 
           modalClick.forEach((modal) => {
             modal.addEventListener('click', (e) => {
-              mModal.style.visibility = 'visible';
-              mModal.style.opacity = '1';
-              if (e.target.parentElement.parentElement) {
-                const ACNumber = e.target.parentElement.parentElement.children[0].innerHTML;
-                const ACName = e.target.parentElement.parentElement.children[1].innerHTML;
-                const ACBalance = e.target.parentElement.parentElement.children[2].innerHTML;
-                accountName.forEach((name) => {
-                  name.innerHTML = ACName;
-                });
-                accountNumber.forEach((number) => {
-                  number.innerHTML = ACNumber;
-                });
-                accountBalance.forEach((balance) => {
-                  balance.innerHTML = ACBalance;
-                });
+              const ACNumber = e.target.parentElement.parentElement.children[0].innerHTML;
+              const ACName = e.target.parentElement.parentElement.children[1].innerHTML;
+              const ACBalance = e.target.parentElement.parentElement.children[2].innerHTML;
+              // CHECK IF PATCH BUTTON IS CLICKED
+              if (e.target.id === 'show-modal') {
+                onDelete.style.display = 'none'; // HIDE DELETE
+                onPatch.style.display = 'block'; // SHOW PATCH
+                mModal.style.visibility = 'visible';
+                mModal.style.opacity = '1';
+                if (e.target.parentElement.parentElement) {
+                  accountName.forEach((name) => {
+                    name.innerHTML = ACName;
+                  });
+                  accountNumber.forEach((number) => {
+                    number.innerHTML = ACNumber;
+                  });
+                  accountBalance.forEach((balance) => {
+                    balance.innerHTML = ACBalance;
+                  });
 
-                submit.addEventListener('click', () => {
-                  e.preventDefault();
-                  patchApi(`${api}/v1/accounts/${ACNumber}`, { status: formSelect.value });
-                });
+                  submit.addEventListener('click', () => {
+                    e.preventDefault();
+                    patchApi(`${api}/v1/accounts/${ACNumber}`, { status: formSelect.value });
+                  });
+                }
+              } else if (e.target.id === 'show-modal2') {
+                onPatch.style.display = 'none'; // HIDE PATCH
+                onDelete.style.display = 'block'; // SHOW DELETE
+                mModal.style.visibility = 'visible';
+                mModal.style.opacity = '1';
+                if (e.target.parentElement.parentElement) {
+                  accountName.forEach((name) => {
+                    name.innerHTML = ACName;
+                  });
+                  accountNumber.forEach((number) => {
+                    number.innerHTML = ACNumber;
+                  });
+
+                  const del = document.querySelector('#delete');
+                  const cancel = document.querySelector('#cancel');
+
+                  del.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    deleteApi(`${api}/v1/accounts/${ACNumber}`);
+                    e.target.parentElement.parentElement.remove();
+                    loader.style.display = 'flex';
+                  });
+
+                  cancel.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    const cModal = document.querySelector(modalIn);
+                    cModal.style.visibility = 'hidden';
+                    cModal.style.opacity = '0';
+                  });
+                }
               }
             });
           });
@@ -182,18 +239,13 @@ const getAccountsApi = (url) => {
       };
 
       modalFunction(showModal, '.modal', '#modal');
+      modalFunction(showModal2, '.modal', '#modal');
 
-      // DELETE
-      const dels = document.querySelectorAll('.deactivate-btn');
-      dels.forEach((del) => {
-        del.addEventListener('click', (e) => {
-          const ACNumber = e.target.parentElement.parentElement.children[0].innerHTML;
-          deleteApi(`${api}/v1/accounts/${ACNumber}`);
-          e.target.parentElement.parentElement.remove();
-        });
-      });
+      return data1;
     });
 };
+
+loader.style.display = 'flex';
 getAccountsApi(`${api}/v1/accounts?limit=${limitSelect.value}`);
 
 // GET FETCH API REQUEST TO GET A SPECIFIC ACCOUNT OF A USER
@@ -212,52 +264,72 @@ const getSpecficAccountApi = (url) => {
   })
     .then((response) => {
       if (response.status === 403) {
+        loader.style.display = 'none';
+        userTable.style.display = 'none';
+        queryForm.style.display = 'none';
         errMsg.innerHTML = 'you must be logged in to view accounts';
       } else {
+        loader.style.display = 'none';
         errMsg.innerHTML = '';
+        userTable.style.display = 'block';
+        queryForm.style.display = 'flex';
         return response.json();
       }
     })
     .then((data1) => {
-      const i = data1.data;
-      const table = document.querySelector('.table');
-      const tableRow = document.createElement('div');
-      tableRow.classList = 'table-row table-body';
-      const accountNumber = document.createElement('div');
-      accountNumber.className = 'account-index';
-      accountNumber.innerHTML = i.accountnumber;
-      const accountName = document.createElement('div');
-      accountName.className = 'account-name';
-      accountName.innerHTML = `${i.firstname} ${i.lastname}`;
-      const status = document.createElement('div');
-      status.className = 'account-status';
-      status.innerHTML = i.status;
-      const activate = document.createElement('div');
-      activate.className = 'activate';
-      const aActivate = document.createElement('a');
-      aActivate.href = '#';
-      aActivate.className = 'activate-btn';
-      aActivate.id = 'show-modal';
-      aActivate.innerHTML = 'Active/Dormant';
-      activate.appendChild(aActivate);
-      const deleteAccount = document.createElement('div');
-      deleteAccount.className = 'deactivate';
-      const aDeleteAccount = document.createElement('a');
-      aDeleteAccount.href = '#';
-      aDeleteAccount.className = 'deactivate-btn';
-      aDeleteAccount.innerHTML = 'Delete';
-      deleteAccount.appendChild(aDeleteAccount);
-      tableRow.appendChild(accountNumber);
-      tableRow.appendChild(accountName);
-      tableRow.appendChild(status);
-      tableRow.appendChild(activate);
-      tableRow.appendChild(deleteAccount);
-      table.appendChild(tableRow);
+      if (data1.status === 404) {
+        loader.style.display = 'none';
+        errMsg.parentElement.style.display = 'flex';
+        errMsg.innerHTML = 'no account found';
+        userTable.style.display = 'none';
+        queryForm.style.display = 'none';
+      } else {
+        errMsg.parentElement.style.display = 'none';
+        loader.style.display = 'none';
+        userTable.style.display = 'block';
+        queryForm.style.display = 'flex';
+        const i = data1.data;
+        const table = document.querySelector('.table');
+        const tableRow = document.createElement('div');
+        tableRow.classList = 'table-row table-body';
+        const accountNumber = document.createElement('div');
+        accountNumber.className = 'account-index';
+        accountNumber.innerHTML = i.accountnumber;
+        const accountName = document.createElement('div');
+        accountName.className = 'account-name';
+        accountName.innerHTML = `${i.firstname} ${i.lastname}`;
+        const status = document.createElement('div');
+        status.className = 'account-status';
+        status.innerHTML = i.status;
+        const activate = document.createElement('div');
+        activate.className = 'activate';
+        const aActivate = document.createElement('a');
+        aActivate.href = '#';
+        aActivate.className = 'activate-btn';
+        aActivate.id = 'show-modal';
+        aActivate.innerHTML = 'Active/Dormant';
+        activate.appendChild(aActivate);
+        const deleteAccount = document.createElement('div');
+        deleteAccount.className = 'deactivate';
+        const aDeleteAccount = document.createElement('a');
+        aDeleteAccount.href = '#';
+        aDeleteAccount.className = 'deactivate-btn';
+        aDeleteAccount.id = 'show-modal2';
+        aDeleteAccount.innerHTML = 'Delete';
+        deleteAccount.appendChild(aDeleteAccount);
+        tableRow.appendChild(accountNumber);
+        tableRow.appendChild(accountName);
+        tableRow.appendChild(status);
+        tableRow.appendChild(activate);
+        tableRow.appendChild(deleteAccount);
+        table.appendChild(tableRow);
+      }
       return data1;
     })
     .then(() => {
       // modal
       const showModal = document.querySelectorAll('#show-modal');
+      const showModal2 = document.querySelectorAll('#show-modal2');
       const accountName = document.querySelectorAll('#username');
       const accountNumber = document.querySelectorAll('#acnumber');
       const accountBalance = document.querySelectorAll('#balance');
@@ -265,31 +337,66 @@ const getSpecficAccountApi = (url) => {
       const modalFunction = (modalClick, modalIn, modalId) => {
         if (modalClick) {
           const mModal = document.querySelector(modalIn);
+          const onPatch = document.querySelector('.onPatch');
+          const onDelete = document.querySelector('.onDelete');
 
           modalClick.forEach((modal) => {
             modal.addEventListener('click', (e) => {
-              mModal.style.visibility = 'visible';
-              mModal.style.opacity = '1';
-              if (e.target.parentElement.parentElement) {
-                const ACNumber = e.target.parentElement.parentElement.children[0].innerHTML;
-                const ACName = e.target.parentElement.parentElement.children[1].innerHTML;
-                const ACBalance = e.target.parentElement.parentElement.children[2].innerHTML;
-                accountName.forEach((name) => {
-                  name.innerHTML = ACName;
-                });
-                accountNumber.forEach((number) => {
-                  number.innerHTML = ACNumber;
-                });
-                accountBalance.forEach((balance) => {
-                  balance.innerHTML = ACBalance;
-                });
+              const ACNumber = e.target.parentElement.parentElement.children[0].innerHTML;
+              const ACName = e.target.parentElement.parentElement.children[1].innerHTML;
+              const ACBalance = e.target.parentElement.parentElement.children[2].innerHTML;
+              // CHECK IF PATCH BUTTON IS CLICKED
+              if (e.target.id === 'show-modal') {
+                onDelete.style.display = 'none'; // HIDE DELETE
+                onPatch.style.display = 'block'; // SHOW PATCH
+                mModal.style.visibility = 'visible';
+                mModal.style.opacity = '1';
+                if (e.target.parentElement.parentElement) {
+                  accountName.forEach((name) => {
+                    name.innerHTML = ACName;
+                  });
+                  accountNumber.forEach((number) => {
+                    number.innerHTML = ACNumber;
+                  });
+                  accountBalance.forEach((balance) => {
+                    balance.innerHTML = ACBalance;
+                  });
 
-                submit.addEventListener('click', () => {
-                  e.preventDefault();
-                  patchApi(`${api}/v1/accounts/${ACNumber}`, { status: formSelect.value });
-                });
+                  submit.addEventListener('click', () => {
+                    e.preventDefault();
+                    patchApi(`${api}/v1/accounts/${ACNumber}`, { status: formSelect.value });
+                  });
+                }
+              } else if (e.target.id === 'show-modal2') {
+                onPatch.style.display = 'none'; // HIDE PATCH
+                onDelete.style.display = 'block'; // SHOW DELETE
+                mModal.style.visibility = 'visible';
+                mModal.style.opacity = '1';
+                if (e.target.parentElement.parentElement) {
+                  accountName.forEach((name) => {
+                    name.innerHTML = ACName;
+                  });
+                  accountNumber.forEach((number) => {
+                    number.innerHTML = ACNumber;
+                  });
 
-                console.log(e.target);
+                  const del = document.querySelector('#delete');
+                  const cancel = document.querySelector('#cancel');
+
+                  del.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    deleteApi(`${api}/v1/accounts/${ACNumber}`);
+                    e.target.parentElement.parentElement.remove();
+                    loader.style.display = 'flex';
+                  });
+
+                  cancel.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    const cModal = document.querySelector(modalIn);
+                    cModal.style.visibility = 'hidden';
+                    cModal.style.opacity = '0';
+                  });
+                }
               }
             });
           });
@@ -306,16 +413,7 @@ const getSpecficAccountApi = (url) => {
       };
 
       modalFunction(showModal, '.modal', '#modal');
-
-      // DELETE
-      const dels = document.querySelectorAll('.deactivate-btn');
-      dels.forEach((del) => {
-        del.addEventListener('click', (e) => {
-          const ACNumber = e.target.parentElement.parentElement.children[0].innerHTML;
-          deleteApi(`${api}/v1/accounts/${ACNumber}`);
-          e.target.parentElement.parentElement.remove();
-        });
-      });
+      modalFunction(showModal2, '.modal', '#modal');
     });
 };
 
@@ -326,6 +424,7 @@ go.addEventListener('click', (e) => {
     tableBody.remove();
   });
 
+  loader.style.display = 'flex';
   getAccountsApi(`${api}/v1/accounts?limit=${limitSelect.value}`);
 });
 
@@ -338,5 +437,6 @@ search.addEventListener('keyup', (e) => {
     tableBody.remove();
   });
 
+  loader.style.display = 'flex';
   getSpecficAccountApi(`${api}/v1/accounts/${search.value}`);
 });
